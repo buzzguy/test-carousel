475f84896fa15335f1a08ee03c6cced43c6fe16e
ethanryan <ethanryan@gmail.com>
2019-02-08 17:14:00 -0500

    added AppContainerWrapper, a styled div

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 class AppContainer extends Component {
   render() {
     return (
-      <div className="AppContainer">
+      <AppContainerWrapper>
         <h1>hello from AppContainer</h1>
-      </div>
+      </AppContainerWrapper>
     );
   }
 }
 
 export default AppContainer;
+
+const AppContainerWrapper = styled.div`
+  border: 3px solid red;
+  margin: 20px;
+`;
