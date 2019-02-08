3ea585ae3e9418e1c952da35cdd9c72226c6705d
ethanryan <ethanryan@gmail.com>
2019-02-08 17:12:55 -0500

    added component, AppContainer

---
+++ src/components/AppContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <div className="AppContainer">
+        <h1>hello from AppContainer</h1>
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
