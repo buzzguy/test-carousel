41da16de783b407c8b78cfa549ce42328348b488
ethanryan <ethanryan@gmail.com>
2019-02-09 10:50:29 -0500

    adding lion image to assets folder

--- src/components/NukaCarousel.js
+++ src/components/NukaCarousel.js
@@ -2,11 +2,23 @@ import React from "react";
 import Carousel from "nuka-carousel";
 import styled from "styled-components";
 
+import lion from "../assets/lion.jpg";
+
 class NukaCarousel extends React.Component {
+  state = {
+    slideIndex: 0
+  }; //adding state
+
   render() {
     return (
       <NukaCarouselWrapper>
-        <Carousel wrapAround="true">
+        <Carousel
+          // wrapAround="true"
+          heightMode={"max"} //is this working???
+          slideIndex={this.state.slideIndex} //adding this with state above
+          afterSlide={slideIndex => this.setState({ slideIndex })} //adding this with state above
+        >
+          <img src={lion} alt="" />
           <img
             src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"
             alt=""
@@ -41,4 +53,9 @@ export default NukaCarousel;
 
 const NukaCarouselWrapper = styled.div`
   border: 2px solid blue;
+  max-height: 200px;
+  .slider {
+    /* width: 50% !important; */
+    height: 175px !important;
+  }
 `;
