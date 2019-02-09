9799769ffc0424b6586f5ed488d3fa9be4553f0b
ethanryan <ethanryan@gmail.com>
2019-02-09 11:17:55 -0500

    setting max-height and object-fit of .slide-slide img

--- src/components/NukaCarousel.js
+++ src/components/NukaCarousel.js
@@ -18,7 +18,7 @@ class NukaCarousel extends React.Component {
           slideIndex={this.state.slideIndex} //adding this with state above
           afterSlide={slideIndex => this.setState({ slideIndex })} //adding this with state above
         >
-          <img src={lion} alt="" />
+          <img src={lion} alt="lion pic" id="lion-king" />
           <img
             src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"
             alt=""
@@ -53,9 +53,14 @@ export default NukaCarousel;
 
 const NukaCarouselWrapper = styled.div`
   border: 2px solid blue;
+  background: lightgray;
   max-height: 200px;
   .slider {
     /* width: 50% !important; */
     height: 175px !important;
+  }
+  .slider-slide > img {
+    max-height: 200px;
+    object-fit: contain;
   }
 `;
