31912961030fd0597eca07459bb46b59ce8c1df0
ethanryan <ethanryan@gmail.com>
2019-02-24 14:02:17 -0500

    mapping over slides array in carousel

--- src/components/ReactResponsiveCarousel/HomepageCarousel.js
+++ src/components/ReactResponsiveCarousel/HomepageCarousel.js
@@ -6,6 +6,131 @@ import homepageImage from '../../assets/homepage-image.jpg'
 import lionPic from '../../assets/lion.jpg'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
+const slides = [
+  {
+    src: homepageImage,
+    alt:
+      'The Living Room - High ceilings with big windows, beautiful lighting and mid century furniture.',
+    caption:
+      'The Living Room - High ceilings with big windows, beautiful lighting and mid century furniture.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: lionPic,
+    alt: 'The Living Room - a lion lives here.',
+    caption: 'The Living Room - a lion lives here.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-1.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-2.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-3.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-4.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-5.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-6.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-7.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-8.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-9.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+  {
+    src: 'http://lorempixel.com/output/cats-q-c-640-480-10.jpg',
+    alt: 'The Living Room - also this cat.',
+    caption: 'The Living Room - also this cat.',
+    address: '524 East 72nd Street, #28A',
+    bed: 2,
+    bath: 2,
+    price: 999999,
+    imageType: 'listing',
+  },
+]
+
 const HomepageCarousel = () => {
   return (
     <HomepageCarouselWrapper>
@@ -16,84 +141,12 @@ const HomepageCarousel = () => {
         showThumbs={false}
         infiniteLoop
       >
-        <div>
-          <img src={homepageImage} alt="" />
-          <p className="legend">HomepageImage</p>
-        </div>
-        <div>
-          <img src={lionPic} alt="" />
-          <p className="legend">LionPic</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 3</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 4</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 5</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-6.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 6</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-7.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 7</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-8.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 8</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 9</p>
-        </div>
-        <div>
-          <img
-            src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg"
-            alt=""
-          />
-          <p className="legend">Legend 10</p>
-        </div>
+        {slides.map(eachSlide => (
+          <div>
+            <img src={eachSlide.src} alt={eachSlide.alt} />
+            <p className="legend">{eachSlide.caption}</p>
+          </div>
+        ))}
       </Carousel>
     </HomepageCarouselWrapper>
   )
@@ -113,7 +166,7 @@ const HomepageCarouselWrapper = styled.div`
   }
 
   .carousel .slide img {
-    max-height: 200px; /* change this to whatever you want */
+    max-height: 300px; /* change this to whatever you want */
     width: auto;
   }
 `
