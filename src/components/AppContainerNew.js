df523848f6cfc2f14aa85c529bb9848202d0f317
ethanryan <ethanryan@gmail.com>
2019-02-25 22:47:25 -0500

    adding HomepageCarousel to AppContainerNew

--- src/components/AppContainerNew.js
+++ src/components/AppContainerNew.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import TestCarouselContainer from './TestCarouselContainer';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import TestCarouselContainer from './TestCarouselContainer'
+import HomepageCarousel from './ReactResponsiveCarousel/HomepageCarousel'
 
 class AppContainerNew extends Component {
   render() {
@@ -8,15 +9,18 @@ class AppContainerNew extends Component {
       <AppContainerWrapper>
         <p>0. TestCarouselForLDP:</p>
         <TestCarouselContainer />
+
+        <p>1. HomepageCarousel:</p>
+        <HomepageCarousel />
       </AppContainerWrapper>
-    );
+    )
   }
 }
 
-export default AppContainerNew;
+export default AppContainerNew
 
 const AppContainerWrapper = styled.div`
   /* background: limegreen; */
   border: 1px solid black;
   /* margin: 20px; */
-`;
+`
